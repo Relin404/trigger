b2e0903c9275689e75a9e90f3f6e2b76605aaa74
relin404 <ahmadmashaal01@gmail.com>
2025-05-24 17:26:21 +0300

    feat: integrate GraphQLModule and PrismaModule into AppModule

--- apps/trigger-auth/src/app/app.module.ts
+++ apps/trigger-auth/src/app/app.module.ts
@@ -1,10 +1,17 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PrismaModule } from 'apps/trigger-auth/src/app/prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
