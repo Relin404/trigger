9e325d132fdb8b6a6447b36bff4c8aa8795fc1f3
relin404 <ahmadmashaal01@gmail.com>
2025-05-24 19:40:22 +0300

    feat: import UsersModule, UsersService, and UsersResolver in AppModule

--- apps/trigger-auth/src/app/app.module.ts
+++ apps/trigger-auth/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { PrismaModule } from 'apps/trigger-auth/src/app/prisma/prisma.module';
+import { UsersService } from './users/users.service';
+import { UsersResolver } from './users/users.resolver';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -10,6 +13,7 @@ import { PrismaModule } from 'apps/trigger-auth/src/app/prisma/prisma.module';
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
