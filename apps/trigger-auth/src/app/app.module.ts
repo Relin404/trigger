e39c3b0510fba61d427f83b2098409f6c8bc4ea3
Ahmad Mash'aal <58116735+Relin404@users.noreply.github.com>
2025-05-28 00:05:09 +0300

    Create user module (#2)
    * chore: add prisma, nestjs/graphql, nestjs/apollo, apollo/server, graphql
    
    * chore: add pnpm workspace configuration for built dependencies
    
    * chore: add dependency on generate-types for build target in project.json
    
    * feat: add User model, datasource, and client output in schema.prisma
    
    * feat: add migration files for User table and migration lock configuration
    
    * refactor: remove AppController and AppService along with their tests
    
    * feat: add PrismaModule and PrismaService with initial tests
    
    * feat: integrate GraphQLModule and PrismaModule into AppModule
    
    * feat: update dependencies in package.json and pnpm-lock.yaml for bcryptjs, class-transformer, and class-validator
    
    * feat: initialize nestjs library with basic configuration files and GraphQL support
    
    * feat: add path mapping for @trigger/nestjs in tsconfig.base.json
    
    * feat: add global validation pipe to enhance request validation
    
    * feat: create user input DTO and user model for GraphQL integration
    
    * feat: implement UsersService with user creation and retrieval methods
    
    * feat: add UsersResolver with user creation and retrieval methods
    
    * feat: create UsersModule to encapsulate user-related services and resolvers
    
    * feat: import UsersModule, UsersService, and UsersResolver in AppModule
    
    * fix ci issue
    
    * test commit hooks
    
    * chore: add nestjs/config
    
    * feat: enable passing tests with no tests defined in Jest configuration
    
    * fix: enable build target dependencies and ensure test depends on generate-types
    
    * refactor: retrieve port configuration from ConfigService instead of environment variable
    
    * fix: restore resolver deleted code
    
    * feat: add ConfigModule to AppModule imports

--- apps/trigger-auth/src/app/app.module.ts
+++ apps/trigger-auth/src/app/app.module.ts
@@ -1,10 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule,
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ apps/trigger-auth/src/app/users/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma-clients/trigger-auth';
+import { hash } from 'bcryptjs';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: Prisma.UserCreateInput) {
+    // Logic to create a user
+    return this.prismaService.user.create({
+      data: {
+        ...data,
+        password: await hash(data.password, 10), // Hash the password before saving
+      },
+    });
+  }
+
+  async getUsers() {
+    // Logic to retrieve users
+    return this.prismaService.user.findMany();
+  }
+}
