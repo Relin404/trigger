de6e4d26b7a9a36cf8765a8959e66869d3e06154
relin404 <ahmadmashaal01@gmail.com>
2025-09-04 02:40:13 +0300

    feat: implement authentication guard and current user decorator in UsersResolver

--- apps/trigger-auth/src/app/users/users.resolver.ts
+++ apps/trigger-auth/src/app/users/users.resolver.ts
@@ -1,7 +1,11 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
 import { User } from './models/user.model';
 import { UsersService } from './users.service';
 import { CreateUserInput } from './dtos/create-user.input';
+import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { TokenPayload } from '../auth/dtos/token-payload.interface';
 
 @Resolver(() => User)
 export class UsersResolver {
@@ -12,8 +16,10 @@ export class UsersResolver {
     return this.usersService.createUser(createUserInput);
   }
 
+  @UseGuards(GqlAuthGuard)
   @Query(() => [User], { name: 'users' })
-  async getUsers() {
+  async getUsers(@CurrentUser() { userId }: TokenPayload) {
+    console.log(`Current user ID: ${userId}`);
     return this.usersService.getUsers();
   }
 }
