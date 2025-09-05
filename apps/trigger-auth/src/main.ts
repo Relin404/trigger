2cb1f8e0cda90411c58d83b11d74d9178e312d0f
relin404 <ahmadmashaal01@gmail.com>
2025-09-05 23:25:48 +0300

    feat: integrate gRPC microservice and update TypeScript paths for auth

--- apps/trigger-auth/src/main.ts
+++ apps/trigger-auth/src/main.ts
@@ -7,7 +7,10 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app/app.module';
+import { AUTH_PACKAGE_NAME } from '@trigger/types/proto/auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -29,6 +32,17 @@ async function bootstrap() {
   );
 
   const port = app.get(ConfigService).getOrThrow('PORT');
+  console.log('AUTH_PACKAGE_NAME =', AUTH_PACKAGE_NAME);
+  console.log('Proto path =', join(__dirname, '../../../proto/auth.proto'));
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: AUTH_PACKAGE_NAME,
+      protoPath: join(__dirname, '../../../proto/auth.proto'),
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(port);
 
   Logger.log(
