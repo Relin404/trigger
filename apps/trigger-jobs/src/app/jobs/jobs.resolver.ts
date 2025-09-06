5fecf0dac46da89ac4a50b77765b18057ba88806
relin404 <ahmadmashaal01@gmail.com>
2025-09-06 16:08:08 +0300

    feat: update import paths and add GqlAuthGuard to JobsResolver

--- apps/trigger-jobs/src/app/jobs/jobs.resolver.ts
+++ apps/trigger-jobs/src/app/jobs/jobs.resolver.ts
@@ -1,14 +1,17 @@
+import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { Job } from '../decorators/job.decorator';
 import { JobMetadata } from '../interfaces/job-metadata.interface';
 import { ExecuteJobInput } from './dto/execute-job.input';
 import { JobsService } from './jobs.service';
+import { Job } from './models/job.model';
+import { GqlAuthGuard } from '@trigger/nestjs';
 
 @Resolver()
 export class JobsResolver {
   constructor(private readonly jobService: JobsService) {}
 
   @Query(() => [Job], { name: 'jobs' })
+  @UseGuards(GqlAuthGuard)
   async getJobs(): Promise<JobMetadata[]> {
     return this.jobService.getJobs();
   }
