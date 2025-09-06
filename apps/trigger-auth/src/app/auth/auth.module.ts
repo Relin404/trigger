5bdb14234e97e84e01bca7f4c2e7e7719534cdbd
relin404 <ahmadmashaal01@gmail.com>
2025-09-06 16:10:44 +0300

    feat: update import paths for consistency in AuthModule

--- apps/trigger-auth/src/app/auth/auth.module.ts
+++ apps/trigger-auth/src/app/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
+
 @Module({
   imports: [
     ConfigModule,
@@ -23,5 +25,6 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     UsersModule,
   ],
   providers: [AuthResolver, AuthService, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
