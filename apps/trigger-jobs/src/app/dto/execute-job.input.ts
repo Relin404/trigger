7dd5ffba98e966096b41e97daecd8a29c95d2955
relin404 <ahmadmashaal01@gmail.com>
2025-09-07 18:54:02 +0300

    feat: add GraphQLJSON field type for data in ExecuteJobInput

--- apps/trigger-jobs/src/app/dto/execute-job.input.ts
+++ apps/trigger-jobs/src/app/dto/execute-job.input.ts
@@ -1,9 +1,14 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { IsNotEmpty } from 'class-validator';
+import GraphQLJSON from 'graphql-type-json';
 
 @InputType()
 export class ExecuteJobInput {
   @Field()
   @IsNotEmpty()
   name: string;
+
+  @Field(() => GraphQLJSON)
+  @IsNotEmpty()
+  data: object | object[];
 }
