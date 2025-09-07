693fc8618dc1e2e1986bc9b5971b3fc0b840fa24
relin404 <ahmadmashaal01@gmail.com>
2025-09-07 18:56:03 +0300

    feat: implement FibonacciData interface and update FibonacciConsumer to use it

--- apps/trigger-executor/src/app/jobs/fibonacci/fibonacci.consumer.ts
+++ apps/trigger-executor/src/app/jobs/fibonacci/fibonacci.consumer.ts
@@ -1,15 +1,20 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PulsarClient, PulsarConsumer } from '@trigger/pulsar';
-import { Message } from 'pulsar-client';
+import { FibonacciData } from './fibonacci-data.interface';
+import { iterate } from 'fibonacci';
 
 @Injectable()
-export class FibonacciConsumer extends PulsarConsumer implements OnModuleInit {
+export class FibonacciConsumer
+  extends PulsarConsumer<FibonacciData>
+  implements OnModuleInit
+{
   constructor(pulsarClient: PulsarClient) {
     super(pulsarClient, 'Fibonacci');
   }
 
-  protected async onMessage(message: Message): Promise<void> {
-    console.log(`FibonacciConsumer.onMessage`);
-    await this.acknowledge(message);
+  protected async onMessage(message: FibonacciData): Promise<void> {
+    const result = iterate(message.iterations);
+
+    this.logger.log(result);
   }
 }
