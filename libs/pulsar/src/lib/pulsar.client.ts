befd2a4c2d7837019300bc5b6d848049ed5a30e0
relin404 <ahmadmashaal01@gmail.com>
2025-09-07 13:37:38 +0300

    feat: initialize Pulsar library with client and consumer implementations

---
+++ libs/pulsar/src/lib/pulsar.client.ts
@@ -0,0 +1,44 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Client, Consumer, Message, Producer } from 'pulsar-client';
+
+@Injectable()
+export class PulsarClient implements OnModuleDestroy {
+  private readonly client = new Client({
+    serviceUrl: this.configService.getOrThrow<string>('PULSAR_SERVICE_URL'),
+  });
+
+  private readonly producers: Producer[] = [];
+  private readonly consumers: Consumer[] = [];
+
+  constructor(private readonly configService: ConfigService) {}
+
+  async onModuleDestroy() {
+    for (const producer of this.producers) await producer.close();
+    for (const consumer of this.consumers) await consumer.close();
+
+    await this.client.close();
+  }
+
+  async createProducer(topic: string) {
+    const producer = await this.client.createProducer({
+      topic,
+    });
+
+    this.producers.push(producer);
+
+    return producer;
+  }
+
+  async createConsumer(topic: string, listener: (message: Message) => void) {
+    const consumer = await this.client.subscribe({
+      topic,
+      subscription: 'trigger',
+      listener,
+    });
+
+    this.consumers.push(consumer);
+
+    return consumer;
+  }
+}
---
+++ libs/pulsar/src/lib/pulsar.consumer.ts
@@ -0,0 +1,24 @@
+import { PulsarClient } from './pulsar.client';
+import { Consumer, Message } from 'pulsar-client';
+
+export abstract class PulsarConsumer {
+  private consumer!: Consumer;
+
+  constructor(
+    private readonly pulsarClient: PulsarClient,
+    private readonly topic: string
+  ) {}
+
+  async onModuleInit() {
+    this.consumer = await this.pulsarClient.createConsumer(
+      this.topic,
+      this.onMessage.bind(this)
+    );
+  }
+
+  protected async acknowledge(message: Message) {
+    await this.consumer.acknowledge(message);
+  }
+
+  protected abstract onMessage(message: Message): Promise<void>;
+}
