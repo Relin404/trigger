e57975b1a378f32303914fae237ffd413960680a
relin404 <ahmadmashaal01@gmail.com>
2025-09-07 18:55:09 +0300

    fix: update import path for FibonacciData and add messageClass property in FibonacciJob

--- apps/trigger-jobs/src/app/jobs/fibonacci/fibonacci.job.ts
+++ apps/trigger-jobs/src/app/jobs/fibonacci/fibonacci.job.ts
@@ -1,13 +1,15 @@
 import { PulsarClient } from '@trigger/pulsar';
 import { Job } from '../../decorators/job.decorator';
 import { AbstractJob } from '../abstract.job';
-import { FibonacciData } from './fibonacci-data.interface';
+import { FibonacciData } from './fibonacci-data.message';
 
 @Job({
   name: 'Fibonacci',
   description: 'Generates a Fibonacci sequence and stores it in the database.',
 })
 export class FibonacciJob extends AbstractJob<FibonacciData> {
+  protected messageClass = FibonacciData;
+
   constructor(pulsarClient: PulsarClient) {
     super(pulsarClient);
   }
