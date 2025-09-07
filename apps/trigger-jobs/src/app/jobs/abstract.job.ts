7a1564a64789512975c73bf311485ec0345cff77
relin404 <ahmadmashaal01@gmail.com>
2025-09-07 13:38:31 +0300

    feat: implement abstract job execution with Pulsar producer integration

--- apps/trigger-jobs/src/app/jobs/abstract.job.ts
+++ apps/trigger-jobs/src/app/jobs/abstract.job.ts
@@ -1,5 +1,17 @@
-export abstract class AbstractJob {
-  execute() {
-    console.log('Executing job...');
+import { Producer } from 'pulsar-client';
+import { PulsarClient } from '@trigger/pulsar';
+
+export abstract class AbstractJob<T> {
+  private producer: Producer;
+
+  constructor(private readonly pulsarClient: PulsarClient) {}
+
+  async execute(data: T, job: string) {
+    if (!this.producer)
+      this.producer = await this.pulsarClient.createProducer(job);
+
+    await this.producer.send({
+      data: Buffer.from(JSON.stringify(data)),
+    });
   }
 }
