ec67b739082181932fec77f8afa8d4696d1b0f15
relin404 <ahmadmashaal01@gmail.com>
2025-09-07 13:35:45 +0300

    refactor: restructure trigger-jobs files

---
+++ apps/trigger-jobs/src/app/jobs/fibonacci/fibonacci.job.ts
@@ -0,0 +1,14 @@
+import { PulsarClient } from '@trigger/pulsar';
+import { Job } from '../../decorators/job.decorator';
+import { AbstractJob } from '../abstract.job';
+import { FibonacciData } from './fibonacci-data.interface';
+
+@Job({
+  name: 'Fibonacci',
+  description: 'Generates a Fibonacci sequence and stores it in the database.',
+})
+export class FibonacciJob extends AbstractJob<FibonacciData> {
+  constructor(pulsarClient: PulsarClient) {
+    super(pulsarClient);
+  }
+}
