333f8b5b3420983f08412bc64d367a9dff05c110
relin404 <ahmadmashaal01@gmail.com>
2025-09-09 16:41:38 +0300

    refactor: rename trigger-executor directory to executor

--- apps/executor/jest.config.ts
+++ apps/executor/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'trigger-executor',
+  displayName: 'executor',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/trigger-executor',
+  coverageDirectory: '../../coverage/apps/executor',
 };
--- apps/executor/webpack.config.js
+++ apps/executor/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/trigger-executor'),
+    path: join(__dirname, '../../dist/apps/executor'),
   },
   plugins: [
     new NxAppWebpackPlugin({
